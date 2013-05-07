02534342594e3bf29e5f443ad82be4b3c36cb6ed
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-05-07 13:08:26 -0700

    Added files to repository

---
+++ Scripts/index.js
@@ -0,0 +1,27 @@
+﻿/*global alert*/
+/*jslint browser:true*/
+(function () {
+	"use strict";
+
+	// Check for the various File API support.
+	if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+		alert('The File APIs are not fully supported in this browser.');
+	}
+
+	function handleFileLoad(evt) {
+		var text = evt.target.result;
+		window.console.log(text);
+	}
+
+	function handleFileSelect(evt) {
+		var file, files, reader;
+		files = evt.target.files; // FileList object
+		file = files[0];
+		reader = new window.FileReader();
+		reader.onload = handleFileLoad;
+
+		reader.readAsText(file);
+	}
+
+	document.getElementById('file').addEventListener('change', handleFileSelect, false);
+}());

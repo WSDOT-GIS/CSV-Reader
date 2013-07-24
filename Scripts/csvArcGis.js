feb8c35caf658385e8a1d2828c29313cc10bb781
JeffJacobson <Jeffrey.M.Jacobson@gmail.com>
2013-07-24 11:40:28 -0700

    csvArcGis: Added function to automatically detect X / Y field names.

--- Scripts/csvArcGis.js
+++ Scripts/csvArcGis.js
@@ -1,4 +1,5 @@
 ﻿/*global require*/
+/*jslint white:true*/
 require("./csv", function (CSV) {
 	"use strict";
 
@@ -64,20 +65,64 @@ require("./csv", function (CSV) {
 		return feature;
 	}
 
+	/** Gets the likely X and Y field names from an object.
+	*/
+	function getXYFieldNames(/*{Object}*/ o) {
+		var xRegex, yRegex, output, name;
+
+		output = {
+			xField: null,
+			yField: null
+		};
+
+		xRegex = /x|(?:long(?:itude)?)/i;
+		yRegex = /y|(?:lat(?:itude)?)/i;
+
+		/*jslint forin:true*/
+		for (name in o) {
+			if (output.xField && output.yField) {
+				break;
+			}
+			if (o.hasOwnProperty(name)) {
+				if (xRegex.test(name)) {
+					output.xField = name;
+				} else if (yRegex.test(name)) {
+					output.yField = name;
+				}
+			}
+		}
+		/*jslint forin:false*/
+
+		return output;
+
+
+	}
+
 	return {
 		/** Converts a CSV string into an array of point feature objects. (http://resources.arcgis.com/en/help/arcgis-rest-api/#/Feature_Object/02r3000000n8000000/)
 		@param {String} csv
 		@param {String} delimiter
-		@param {string} xName The field name that provides the X values
-		@param {string} yName The field name that provides the Y values
+		@param {string} [xName] The field name that provides the X values. This can be omitted as long as the X field is named "X", "Long" or "Longitude" (case-insensitive).
+		@param {string} [yName] The field name that provides the Y values.This can be omitted as long as the Y field is named "Y", "Lat" or "Latitude" (case-insensitive).
 		@param {Object} [spatialReference] For values, see http://resources.arcgis.com/en/help/arcgis-rest-api/#/Geometry_Objects/02r3000000n1000000/
 		@param {String} [zName]
 		@param {String} [mName]
 		@return {Array}
 		*/
 		csvToPointFeatures: function (csv, delimiter, xName, yName, spatialReference, zName, mName) {
-			var objects, attributes, point, output = [], i, l, o;
+			var objects, attributes, point, output = [], i, l, o, fieldNames;
 			objects = CSV.toObjects(csv, delimiter);
+
+			// If xName or yName were not provided, determine those values.
+			if (!xName || !yName) {
+				fieldNames = getXYFieldNames(objects[0]);
+				xName = xName || fieldNames.xField;
+				yName = yName || fieldNames.yField;
+			}
+
+			if (!xName || !yName) {
+				throw new TypeError("Either the xName or yName were not provided and could not be determined automatically.");
+			}
 
 			for (i = 0, l = objects.length; i < l; i += 1) {
 				o = objectToPointFeature(objects[i], delimiter, xName, yName, spatialReference, zName, mName);
---
+++ Scripts/map.js
@@ -0,0 +1,41 @@
+﻿/*global require*/
+require(["csv/csvArcGis", "esri/map"], function (csvArcGis, Map) {
+	"use strict";
+
+	var map;
+
+	// Check for the various File API support.
+	if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+		alert('The File APIs are not fully supported in this browser.');
+		return;
+	}
+
+	map = new Map("map", {
+		basemap: "gray",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
+	});
+
+	function handleFileLoad(evt) {
+		var text, features;
+
+		text = evt.target.result;
+
+		features = csvArcGis.csvToPointFeatures(text);
+
+	}
+
+	function handleFileSelect(evt) {
+		var file, files, reader;
+		files = evt.target.files; // FileList object
+		file = files[0];
+		reader = new window.FileReader();
+		reader.onload = handleFileLoad;
+
+		reader.readAsText(file);
+	}
+
+	document.getElementById('file').addEventListener('change', handleFileSelect, false);
+
+});
